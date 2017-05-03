a6d6bf146b35011cfb750281a102045f8a7b2226
Amar Thapa <mrthapa322@gmail.com>
2017-05-03 14:25:40 +0530

    Inital commit, changed name from Curiosity to Vengeance, first compile using clang llvm framework

---
+++ Globals.h
@@ -0,0 +1,63 @@
+/*
+ * Globals.h
+ *
+ *  Created on: May 2, 2017
+ *      Author: Amar Thapa
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+typedef unsigned char u8;
+typedef unsigned int u16;
+typedef unsigned long int u32;
+typedef unsigned long long int u64;
+
+/* structure to hold a move */
+
+struct Move {
+	int to;        /* to square of a move */
+	int from;	   /* from square of a move */
+	int moveType;  /* 0-pushes, 1-captures, 2-castling, 3-enPassant, 4-promotions, 5-doubleMoves */
+	int piece;     /* the piece to move */
+	int color;     /* side to which piece belongs i.e bb containing all pieces of that color*/
+	int cPiece;    /* captured piece */
+	int cColor;    /* side to which the captured piece belongs */
+};
+
+#define MAX_MOVES 256;
+
+/* array size for pieceBB array */
+
+#define SIZE 16
+
+/* pieceBB is an array containing bitboards for all pieces */
+
+u64 pieceBB[SIZE];
+
+/* index for white pieces in pieceBB array */
+
+#define WHITE_KING 0
+#define WHITE_QUEEN 1
+#define WHITE_BISHOPS 2
+#define WHITE_KNIGHTS 3
+#define WHITE_ROOKS 4
+#define WHITE_PAWNS 5
+#define WHITE_PIECES 6
+
+/* index for black pieces in pieceBB array */
+
+#define BLACK_KING 7
+#define BLACK_QUEEN 8
+#define BLACK_BISHOPS 9
+#define BLACK_KNIGHTS 10
+#define BLACK_ROOKS 11
+#define BLACK_PAWNS 12
+#define BLACK_PIECES 13
+
+/* index for occupied and empty squares in pieceBB array */
+
+#define OCCUPIED_SQUARES 14
+#define EMPTY_SQUARES 15
+
+#endif /* GLOBALS_H_ */
---
+++ Main.c
@@ -0,0 +1,51 @@
+/*
+ * Main.c
+ *
+ *  Created on: May 2, 2017
+ *      Author: Amar Thapa
+ */
+
+#include <stdio.h>
+#include "Globals.h"
+
+void init();
+
+int main(int argc, char **argv) {
+
+	init(); /* initalize pieceBB array */
+
+	for(int i = 0; i < SIZE; i++) {
+		
+		printf("%llu\n", pieceBB[i]);
+	}
+
+
+	return 0;
+}
+
+
+void init() {
+
+	/* default positions of pieces on board */
+
+	pieceBB[WHITE_KING] = 0x0000000000000008U;
+	pieceBB[WHITE_QUEEN] = 0x0000000000000010U;
+	pieceBB[WHITE_BISHOPS] = 0x0000000000000024U;
+	pieceBB[WHITE_KNIGHTS] = 0x0000000000000042U;
+	pieceBB[WHITE_ROOKS] = 0x0000000000000081U;
+	pieceBB[WHITE_PAWNS] = 0x000000000000FF00U;
+	pieceBB[WHITE_PIECES] = 0x000000000000FFFFU;
+
+	pieceBB[BLACK_KING] = 0x0800000000000000U;
+	pieceBB[BLACK_QUEEN] = 0x1000000000000000U;
+	pieceBB[BLACK_BISHOPS] = 0x2400000000000000U;
+	pieceBB[BLACK_KNIGHTS] = 0x4200000000000000U;
+	pieceBB[BLACK_ROOKS] = 0x8100000000000000U;
+	pieceBB[BLACK_PAWNS] = 0x00FF000000000000U;
+	pieceBB[BLACK_PIECES] = 0xFFFF000000000000U;
+
+	pieceBB[OCCUPIED_SQUARES] = 0xFFFF00000000FFFFU;
+	pieceBB[EMPTY_SQUARES] = 0x0000FFFFFFFF0000U;
+
+}
+
